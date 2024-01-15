8faa48a02ac03917bf16582786e090b3cc70be11
Sakrid1s <bilinets.vitalik@gmail.com>
2024-01-15 19:13:29 +0200

    done task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('div#boxes');
+const userInput = document.querySelector('input');
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesDiv.appendChild(box);
+    size += 10;
+  }
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = Math.round(userInput.value);
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+    userInput.value = '';
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
