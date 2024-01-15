b38c7605eff74ce9680f6af6d52a5084e852c08b
Sakrid1s <bilinets.vitalik@gmail.com>
2024-01-15 15:26:10 +0200

    Done task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyElem = document.body;
+const colorSpan = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', handleChangeColor);
+
+function handleChangeColor(event) {
+  const newColor = getRandomHexColor();
+  colorSpan.textContent = newColor;
+  bodyElem.style.backgroundColor = newColor;
+}
